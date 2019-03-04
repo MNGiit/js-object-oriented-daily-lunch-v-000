1de6c709f33503fd4715a458d3b0a652798046bf
Mohammed Noori <kmanoorib@gmail.com>
2019-03-04 21:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+// Id for each Class: Neighborhood, Customer, Meal
+let neighborhoodId = 0;
+class Neighborhood {
+  constructor(name) {
+    this.name = name;
+    
+    // add new instance of Neighborhood to store's neighborhoods 
+    store.neighborhoods.push(this);
+  }
+  
+  // returns a list of all deliveries placed in a Neighborhood
+  
+  // returns all of the customers that live in a particular neighborhood
+  
+  // returns a unique list of meals that have been ordered in a particular neighborhood
+}
+
+class Customer {
+  constructor()
+}
