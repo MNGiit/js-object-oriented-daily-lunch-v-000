525b10ce8c2e867f30ffc219af75f056fbcaf00a
Mohammed Noori <kmanoorib@gmail.com>
2019-03-05 02:52:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ class Neighborhood {
   // returns a unique list of meals that have been ordered in a particular neighborhood
   meals() {
     let history = this.customers().map(function(customer) {return customer.meals()});
-    return history.unique;
+    return history.distinct();
   }
 }
 
